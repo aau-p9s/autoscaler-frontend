3010ef5c312ce7aa7833e28703dd017804bf02fa
mast3r@skade.dev <mast3r@skade.dev>
2024-11-22 13:14:59 +0100

    changes from cluster

--- autoscaler-frontend/autoscaler-frontend/Database/PrometheusGenerator.cs
+++ autoscaler-frontend/autoscaler-frontend/Database/PrometheusGenerator.cs
@@ -8,17 +8,16 @@ class PrometheusGenerator {
         var addr = ArgumentParser.Get("--prometheus-addr");
     }
 
-    public async Task<IEnumerable<Tuple<int, string>>> GetMetrics() {
-        var response = await client.GetAsync(build_query());
+    public async Task<IEnumerable<Tuple<double, string>>> GetMetrics() {
+        var query = build_query();
+        var response = await client.GetAsync(query);
         var json = await response.Content.ReadFromJsonAsync<JsonObject>();
-        List<Tuple<int, string>> result_list = new List<Tuple<int, string>>();
+        List<Tuple<double, string>> result_list = new List<Tuple<double, string>>();
         foreach(var result in json["data"]["result"].AsArray()) {
-            foreach(var (x, y) in result["values"].AsArray().Select(value => ((int)value[0].AsValue(), (string)value[1].AsValue()))) {
-                result_list.Add(new Tuple<int, string>(x, y));
+            foreach(var (x, y) in result["values"].AsArray().Select(value => ((double)value[0].AsValue(), (string)value[1].AsValue()))) {
+                result_list.Add(new Tuple<double, string>(x, y));
             }
         }
-        Console.Write("prometheus data points: ");
-        Console.WriteLine(result_list.Count());
         return result_list;
     }
     public string build_query() {
@@ -26,7 +25,7 @@ class PrometheusGenerator {
         var baseQuery = $"{addr}/api/v1/query_range?query=container_network_receive_packets_total%7Bpod%3D~%22stregsystemet.%2A%22%7D";
         var timeNow = DateTime.Now;
         var time7DaysAgo = timeNow.AddDays(-7);
-        return baseQuery + "&start=" + ToRFC3339(time7DaysAgo) + "&end=" + ToRFC3339(timeNow) + "&step=15s";
+        return baseQuery + "&start=" + ToRFC3339(time7DaysAgo) + "&end=" + ToRFC3339(timeNow) + "&step=60s";
     }
 
      private string ToRFC3339(DateTime date)

e75dc6262f1071e9a4d15dc178be2d7eef04504d
Thomas <mast3r@skade.dev>
2024-12-13 15:15:04 +0100

    implemented

--- autoscaler/autoscaler.py
+++ autoscaler/autoscaler.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 from time import sleep
-from json import dumps
+from json import dumps, loads
 from random import randint
 import datetime
+import sys
 
-print(dumps([{
-    "time": (datetime.date.today() + datetime.timedelta(days=i+1)),
+historic = loads(sys.stdin.readline())
+sys.stderr.write(f"{historic}\n")
+# TODO: Machine learning
+# End Machine learning
+
+forecast = [{
+    "time": datetime.datetime.now() + datetime.timedelta(seconds=i*10),
     "value": randint(0, 10)
-} for i in range(7)], default=str))
+} for i in range(7)]
+
+print(dumps(forecast, default=str))
